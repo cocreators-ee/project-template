a189e53ef943d457dc37e9bc39fa8b005df34a4e
Janne Enberg <janne.enberg@lietu.net>
2019-11-01 22:55:16 +0200

    Some basic unit tests

---
+++ devops/tests/test_component.py
@@ -0,0 +1,68 @@
+from io import StringIO
+
+import yaml
+from devops.lib.component import Component
+
+DEPLOYMENT = """
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      app: test-deployment
+  template:
+    metadata:
+      labels:
+        app: test-deployment
+    spec:
+      containers:
+        - name: test-deployment
+          imagePullPolicy: IfNotPresent
+          image: imagined.registry.tld/myproj-service-test-deployment:latest
+"""
+
+
+def get_deployment() -> dict:
+    return yaml.load(StringIO(DEPLOYMENT), yaml.Loader)
+
+
+def test_get_full_docker_name():
+    c = Component("service/test-service")
+    assert c._get_full_docker_name() == "service-test-service:latest"
+
+    c = Component("service/test-service")
+    c.image_prefix = "myproj-"
+    c.tag = "v1.2.3"
+    assert c._get_full_docker_name() == "myproj-service-test-service:v1.2.3"
+
+
+def test_patch_containers():
+    deploy = get_deployment()
+    c = Component("service/test-service")
+    c.image = "test-image"
+    c.tag = "v6.6.6"
+    c._patch_containers(deploy)
+
+    container = deploy["spec"]["template"]["spec"]["containers"][0]
+    assert container["image"] == "test-image:v6.6.6"
+
+
+def test_patch_image_pull_secrets():
+    deploy = get_deployment()
+    c = Component("service/test-service")
+    c.image_pull_secrets = {"imagined.registry.tld": "secret"}
+    c._patch_image_pull_secrets(deploy)
+    spec = deploy["spec"]["template"]["spec"]
+    assert spec["imagePullSecrets"][0]["name"] == "secret"
+
+
+def test_patch_replicas():
+    deploy = get_deployment()
+    c = Component("service/test-service")
+    c.replicas = 77
+    c._patch_replicas(deploy)
+
+    assert deploy["spec"]["replicas"] == 77
--- devops/tests/test_utils.py
+++ devops/tests/test_utils.py
@@ -1,19 +1,71 @@
+import sys
+from importlib import invalidate_caches
+from pathlib import Path
+from shutil import rmtree
+
+import pytest
 from devops.lib.utils import list_envs, load_env_settings, run
 
+TEST_ENV = "unit_test_env_6zxuj"
+TEST_ENV_PATH = Path("envs") / TEST_ENV
+TEST_ENV_SETTINGS = TEST_ENV_PATH / "settings.py"
 
-def test_load_env_settings():
+TEST_SETTINGS = """
+COMPONENTS = ["service/TEST_COMPONENT_LOL"]
+KUBE_CONTEXT = "TEST_CONTEXT_LOL"
+KUBE_NAMESPACE = "TEST_NAMESPACE_LOL"
+"""
+
+TEST_FULL_SETTINGS = (
+    TEST_SETTINGS
+    + """
+REPLICAS = {"service/TEST_COMPONENT_LOL": 3}
+IMAGE_PULL_SECRETS = {"service/TEST_COMPONENT_LOL": "secret"}
+"""
+)
+
+
+def clean_caches():
+    for path in Path(".").rglob("__pycache__"):
+        rmtree(path)
+    invalidate_caches()
+
+
+def delete_test_settings():
+    if TEST_ENV_PATH.exists():
+        rmtree(TEST_ENV_PATH)
+
+
+@pytest.fixture()
+def clean_test_settings():
+    delete_test_settings()
+    yield None
+    delete_test_settings()
+
+
+def test_load_env_settings(clean_test_settings):
     envs = list_envs()
     settings = load_env_settings(envs[0])
-    _ = settings.IMAGE_PULL_SECRETS
-    _ = settings.KUBE_CONTEXT
-    _ = settings.KUBE_NAMESPACE
-    _ = settings.COMPONENTS
-    _ = settings.REPLICAS
+
+    getattr(settings, "IMAGE_PULL_SECRETS")
+    getattr(settings, "KUBE_CONTEXT")
+    getattr(settings, "KUBE_NAMESPACE")
+    getattr(settings, "COMPONENTS")
+    getattr(settings, "REPLICAS")
+
+    TEST_ENV_PATH.mkdir(parents=True)
+    TEST_ENV_SETTINGS.write_text(TEST_SETTINGS)
+
+    settings = load_env_settings(TEST_ENV)
+    assert len(settings.COMPONENTS) == 1
+    assert "service/TEST_COMPONENT_LOL" in settings.COMPONENTS
+    assert settings.KUBE_CONTEXT == "TEST_CONTEXT_LOL"
+    assert settings.KUBE_NAMESPACE == "TEST_NAMESPACE_LOL"
+    assert len(settings.IMAGE_PULL_SECRETS) == 0
+    assert len(settings.REPLICAS) == 0
 
 
 def test_run():
-    res = run(["python", "-c", "\"import sys; sys.stdout.write('test')\""])
-    assert res.stdout == "test"
-
-    res = run(["python", "-c", "\"import sys; sys.stderr.write('test')\""])
-    assert res.stderr == "test"
+    res = run(["python", "--version"])
+    ver = sys.version.split(" ")[0]
+    assert res.stdout.decode("utf-8").strip() == f"Python {ver}"
