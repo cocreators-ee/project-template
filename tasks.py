de7bfeb1ad2d9bc90e910c3bd3ee8b104acf5649
Egor Danchenkov <egor.danchenkov@rednuclearmonkey.com>
2020-01-21 12:11:44 +0200

    Ensure namespace

--- tasks.py
+++ tasks.py
@@ -125,6 +125,7 @@ def init_kubernetes(ctx, env):
 
     settings = load_env_settings(env)
     devops.tasks.ensure_context(settings.KUBE_CONTEXT)
+    devops.tasks.ensure_namespace(settings.KUBE_NAMESPACE)
 
     def _apply(config, **kwargs):
         run(["kubectl", "apply", "-f", config], **kwargs)
