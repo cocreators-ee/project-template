753d23f45edba80c1cc0c8006f43511a5ef37ec8
Joakim Nordling <joakim.nordling@gmail.com>
2020-02-15 10:57:27 +0200

    Remove unused imports

--- devops/lib/utils.py
+++ devops/lib/utils.py
@@ -2,12 +2,9 @@ import importlib
 import subprocess  # nosec
 import types
 from copy import deepcopy
-from io import StringIO
 from pathlib import Path
 from time import time
 from typing import Callable, List, Optional, Union
-
-import yaml
 
 from devops.lib.log import logger
 
